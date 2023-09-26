71db0d8cee031b4b3fbd8b35aa53e672b0bae7a8
Arthur Perton <arthur@webenapp.nl>
2023-09-26 12:20:06 +0200

    Fix routes

--- routes/cp.php
+++ routes/cp.php
@@ -1,15 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Statamic\Addons\WordpressUsers\Http\Controllers\Controller;
 
-// Route::namespace('\Statamic\Addons\WordpressUsers\Http\Controllers')
-//     ->prefix('wordpress-users/')
 Route::prefix('wordpress-users/')
     ->name('wordpress-users.')
     ->group(function () {
-        Route::get('', 'Controller@index')->name('index');
-        Route::get('edit/{step}', 'Controller@edit')->name('edit');
-        Route::patch('update/{step}', 'Controller@update')->name('update');
-        Route::get('import', 'Controller@import')->name('import');
-        Route::get('valid', 'Controller@valid')->name('valid');
+        Route::get('', [Controller::class, 'index'])->name('index');
+        Route::get('edit/{step}', [Controller::class, 'edit'])->name('edit');
+        Route::patch('update/{step}', [Controller::class, 'update'])->name('update');
+        Route::get('import', [Controller::class, 'import'])->name('import');
+        Route::get('valid', [Controller::class, 'valid'])->name('valid');
     });
